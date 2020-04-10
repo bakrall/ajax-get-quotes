c2234ec5ea4cdafffc24a430c5e87d83577e82fa
Barbara Stadnik <barbara1stadnik@gmail.com>
2020-04-10 18:20:17 +0200

    create initial files, take security measures

---
+++ gulpfile.js
@@ -0,0 +1,6 @@
+const gulp = require('gulp'),
+	connect = require('gulp-connect');
+
+const runServer = () => connect.server();
+
+exports.default = runServer;
---
+++ public/numbers.js
@@ -0,0 +1,37 @@
+//https://pprathameshmore.github.io/QuoteGarden/
+
+(function() {
+	'use strict';
+
+	const url = 'https://quote-garden.herokuapp.com/api/v2/quotes/random';
+
+	function HtmlEncode(s) {
+		var HTMLCharMap = {
+			"&" : "&amp;",
+			"'" : "&#39;",
+			'"' : "&quot;",
+			"<" : "&lt;",
+			">" : "&gt;",
+			"\\" : "&#x5c;",
+			"`" : "&#x60;",
+			":" : "&#58;"
+		};
+
+		function encodeHTMLmapper(ch) {
+		return HTMLCharMap[ch];
+		}
+
+		return s.replace(/[&'''<>\\`:]/g, encodeHTMLmapper);
+	}
+
+	$.ajax({
+		type: 'GET',
+		url: url,
+		success: function(response) {
+			console.log(HtmlEncode(response.quote.quoteText));
+		},
+		error: function(error) {
+			console.log(error);
+		}
+	});
+})();
